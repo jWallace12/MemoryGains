4740ad870d81877a7ff19583d1731af25f64fe49
jWallace12 <jonah.d.wallace96@gmail.com>
2017-10-31 09:38:27 -0700

    Initial Save

---
+++ app/src/main/java/a2/csci412/wwu/edu/memorygain/MainActivity.java
@@ -0,0 +1,13 @@
+package a2.csci412.wwu.edu.memorygain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/a2/csci412/wwu/edu/memorygain/Settings.java
@@ -0,0 +1,8 @@
+package a2.csci412.wwu.edu.memorygain;
+
+/**
+ * Created by Jonah on 10/31/2017.
+ */
+
+public class Settings {
+}
