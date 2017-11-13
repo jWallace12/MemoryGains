db62886c9d0be68d7eaf61a9f4ed5bec9c0c0989
jWallace12 <jonah.d.wallace96@gmail.com>
2017-11-13 15:52:12 -0800

    Fixing app dependencies

--- app/src/main/java/a2/csci412/wwu/edu/memorygain/LocationRecall.java
+++ app/src/main/java/a2/csci412/wwu/edu/memorygain/LocationRecall.java
@@ -1,5 +1,6 @@
 package a2.csci412.wwu.edu.memorygain;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,6 +14,11 @@ public class LocationRecall extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_recall);
+    }
+
+    public void recordLocation( View v ) {
+        Intent myIntent = new Intent(this, MapsActivity.class);
+        this.startActivity(myIntent);
     }
 
     public void goBack( View v ) {
